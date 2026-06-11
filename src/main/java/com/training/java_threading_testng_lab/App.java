681752c6757404bbbd51cb8335ad48642aaf591f
canisha13 <canisha.subedi@gmail.com>
2026-06-11 14:52:22 +0100

    Initial Maven project setup

---
+++ src/main/java/com/training/java_threading_testng_lab/App.java
@@ -0,0 +1,13 @@
+package com.training.java_threading_testng_lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
